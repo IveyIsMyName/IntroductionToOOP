bed8c20e6550da0ae872731332e27c209c9f83d7
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2024-06-22 08:55:30 -0400

    String in progress

---
+++ String/main.cpp
@@ -0,0 +1,105 @@
+﻿#include<iostream>
+using namespace std;
+#define delimiter "\n--------------------------------------------------------------------\n"
+
+class String
+{
+	int size;
+	char* str;
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
+	{
+		return str;
+	}
+	char* get_str()
+	{
+		return str;
+	}
+	
+	//				Constructors:
+	String(const char* str)
+	{
+		this->size = strlen(str) + 1;
+		this->str = new char[size] {};
+		for (int i = 0; str[i]; i++)
+		{
+			this->str[i] = str[i];
+		}
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	explicit String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "1ArgConstructor:\t" << this << endl;
+	}
+	
+	String(const String& other)
+	{
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)
+		{
+			this->str[i] = other.str[i];
+		}
+		cout << "CopyConstructor:\t" << this << endl;
+	}
+	~String()
+	{
+		delete[] str;
+		cout << "Destructor:\t\t" << this << endl;
+	}
+
+	//				 Operators:
+	String& operator=(const String& other)
+	{
+		delete[] this->str;
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+	
+	//				  Methods:
+	void print()const
+	{
+		cout << "String:\t" << str << endl;
+		cout << "Size:\t" << size << endl;
+	}
+};
+
+std::ostream& operator<<(std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	
+	String str1;
+	str1.print();
+	cout << delimiter << endl;
+
+	String str2(8);
+	str2.print();
+	cout << delimiter << endl;
+
+	String str3 = "Hello"; 
+	str3.print();
+	cout << delimiter << endl;
+
+	String str4 = "World";
+	cout << "String: " << str4 << endl;
+	cout << delimiter << endl;
+
+	str4 = str3;
+	str4.print();
+	cout << delimiter << endl;
+
+}
