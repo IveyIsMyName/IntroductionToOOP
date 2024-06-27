cb3c21dcf34b308af01da225f330b365790da55b
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2024-06-27 14:48:23 -0400

    HeaderInitialization DONE

--- String/main.cpp
+++ String/main.cpp
@@ -1,8 +1,8 @@
 ﻿#include<iostream>
 using namespace std;
-#define delimiter "\n--------------------------------------------------------------------\n"
 using std::cin;
 using std::cout;
+#define delimiter "\n--------------------------------------------------------------------\n"
 
 class String
 {
@@ -23,38 +23,38 @@ public:
 	}
 
 	//				Constructors:
-	String(const char* str)
+	String(const char* str):size(strlen(str) + 1), str(new char[size]{})
 	{
-		this->size = strlen(str) + 1;
-		this->str = new char[size] {};
+		//this->size = strlen(str) + 1;
+		//this->str = new char[size] {};
 		for (int i = 0; str[i]; i++)
 		{
 			this->str[i] = str[i];
 		}
 		cout << "Constructor:\t\t" << this << endl;
 	}
-	explicit String(int size = 80)
+	explicit String(int size = 80):size(size), str(new char[size] {})
 	{
-		this->size = size;
-		this->str = new char[size] {};
+		//this->size = size;
+		//this->str = new char[size] {};
 		cout << "1ArgConstructor:\t" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other):size(other.size), str(new char[size]{})
 	{
 		//Deep Copy (Побитовое копирование)
-		this->size = other.size;
-		this->str = new char[size] {};
+		//this->size = other.size;
+		//this->str = new char[size] {};
 		for (int i = 0; i < size; i++)
 		{
 			this->str[i] = other.str[i];
 		}
 		cout << "CopyConstructor:\t" << this << endl;
 	}
-	String(String&& other)noexcept //r-value reference
+	String(String&& other)noexcept:size(other.size), str(other.str) //r-value reference
 	{
 		//Shallow copy:
-		this->size = other.size;
-		this->str = other.str;
+		//this->size = other.size;
+		//this->str = other.str;
 
 		//Reset other:
 		other.size = 0;
@@ -105,6 +105,7 @@ public:
 		cout << "String:\t\t\t" << str << endl;
 		cout << "Addr\t\t\t" << &str << endl;
 		cout << "Size:\t\t\t" << size << endl;
+		cout << delimiter << endl;
 	}
 };
 std::ostream& operator<<(std::ostream& os, const String& obj)
@@ -125,7 +126,8 @@ String operator+(const String& left, const String& right)
 }
 //#define CONSTRUCTORS_CHECK
 //#define OPERATOR_PLUS_CHECK
-#define OPERATOR_MOVE_ASSIGNMENT
+//#define OPERATOR_MOVE_ASSIGNMENT
+#define CALLING_CONSTRUCTORS
 
 void main()
 {
@@ -187,5 +189,33 @@ void main()
 	cout << delimiter << endl;
 	cout << str3 << endl;
 #endif // OPERATOR_MOVE_ASSIGNMENT
+
+#ifdef CALLING_CONSTRUCTORS
+	String str1; //Default constructor
+	str1.print();
+
+	//String str2 = 8; //Single-Argument construcor 'int', explicit вызвать невозможно
+	String str2(8); //explicit constructor можно вызвать только так
+	str2.print();
+
+	String str3 = "Hello"; //Single-Argument constructor 'char'
+	str3.print();
+
+	String str4();			//Здесь НЕ вызывается никакой конструкто, и не создается объект,
+							//здесь оъявляется функция str4(), которая не принимает никаких параметров
+							//и возвращается значение типа "String"
+							//т.е., таким образом DefaultConstructor вызвать невозможно.
+	//str4.print();
+	//Если нужно вызвать DefaultConstructor, это делается следующим образом:
+	//String str6 = str3;		//CopyConstructor
+	//String str6(str3);		//CopyConstructor
+	String str6{ str3 };		//CopyConstructor
+	str6.print();
+	String str5{};			//Явный вызов конструтора по умолчанию
+	str5.print();
+	String str7 = str5 + str6;
+	str7.print();
+	//Следовательно, абсолютно любой конструктор можно вызвать при помощи () или {}
+#endif // CALLING_CONSTRUCTORS
 
 }
